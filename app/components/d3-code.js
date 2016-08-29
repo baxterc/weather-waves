fac319482eff599679545c11c3080d086f52e3c0
Colin Kingen <colinwkingen@gmail.com>
2016-08-29 13:24:26 -0700

    begin adding random generation of scatter plot data

--- app/components/d3-code.js
+++ app/components/d3-code.js
@@ -1,18 +1,31 @@
 import Ember from 'ember';
+import d3 from 'd3';
 
 export default Ember.Component.extend({
-  didInsertElement() {
-    let svgContainer = d3.select('#holder').append('svg').attr('width',700).attr('height',700);
 
+
+
+
+  didInsertElement() {
+    let svgContainer = d3.select('#holder').append('svg').attr('width',600).attr('height',600);
     svgContainer.append('circle')
-    .attr('cx',250)
-    .attr('cy',250)
-    .attr('r', 100)
+    .attr('cx',125)
+    .attr('cy',100)
+    .attr('r', 80)
+    .style("stroke", "orange")
+    .style('fill','blue')
     .transition()
     .attr('cx',500)
     .attr('cy',450)
     .duration(2000)
     .style('fill','red');
-
   }
 });
+
+var dataset = [
+                [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+                [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+              ];
+
+var ramdomInt = Math.random(Math.floor() * 500);
+var randomData =
