ac46916cc8051f57e7ed1445ed4a32d44831a577
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-29 13:24:13 -0700

    merge api-call changes

--- app/components/d3-code.js
+++ app/components/d3-code.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+import d3 from 'd3';
+
 
 export default Ember.Component.extend({
+
   didInsertElement() {
     let svgContainer = d3.select('#holder').append('svg').attr('width',700).attr('height',700);
 
@@ -8,11 +11,17 @@ export default Ember.Component.extend({
     .attr('cx',250)
     .attr('cy',250)
     .attr('r', 100)
+    .style('fill','blue')
     .transition()
     .attr('cx',500)
     .attr('cy',450)
     .duration(2000)
     .style('fill','red');
 
+  },
+  actions: {
+    getWeather() {
+      console.log(model);
+    },
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // model: function() {
+  //   var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=47c99ae9b1f7f7412bba0f762a2b3c68';
+  //   return Ember.$.getJSON(url).then(function(responseJSON) {
+  //     return responseJSON;
+  //   });
+  // },
+  actions: {
+    getLoc: function() {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+      });
+    },
+
+  }
 });
