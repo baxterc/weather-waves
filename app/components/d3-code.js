51252944c00aaa6f14759a1d82c3f9ea3974ad04
Chip Carnes <chip.carnes27@gmail.com>
2016-08-30 10:17:16 -0700

    Add nav bar and materialize framework

--- app/components/d3-code.js
+++ app/components/d3-code.js
@@ -4,7 +4,7 @@ import d3 from 'd3';
 export default Ember.Component.extend({
   didInsertElement() {
     let svgContainer = d3.select('#holder').append('svg').attr('width',700).attr('height',700);
-    //
+
     // svgContainer.append('circle')
     // .attr('cx',250)
     // .attr('cy',250)
@@ -14,14 +14,14 @@ export default Ember.Component.extend({
     // .attr('cy',450)
     // .duration(2000)
     // .style('fill','red');
-    //
+
     // var circle = d3.selectAll("circle");
     // circle.style("fill", "steelblue");
     // circle.attr("r", 30);
     // circle.data([32, 57, 112]);
     // circle.attr("r", function(d){return Math.sqrt(d);});
     // circle.attr("cx", function(d, i) { return i * 100 + 30; });
-    var svg = d3.select('svg');
+    // var svg = d3.select('svg');
     //
     // var circle = svg.selectAll("circle").data([32, 57, 112, 293]);
     // var circleEnter = circle.enter().append("circle");
@@ -31,11 +31,16 @@ export default Ember.Component.extend({
     // circleEnter.style('fill', 'steelblue');
 
     // This accomplishes entering data if there's none to begin with on the page
-    svg.selectAll("circle")
-    .data([32, 57, 112, 293])
-    .enter().append("circle")
-    .attr("cy", 60)
-    .attr("cx", function(d, i) { return i * 100 + 30; })
-    .attr("r", function(d) { return Math.sqrt(d); });
+    // svg.selectAll("circle")
+    // .data([32, 57, 112, 293])
+    // .enter().append("circle")
+    // .attr("cy", 60)
+    // .attr("cx", function(d, i) { return i * 100 + 30; })
+    // .attr("r", function(d) { return Math.sqrt(d); });
+    //
+    /* Define an array of objects */
+    
+
+
   }
 });
