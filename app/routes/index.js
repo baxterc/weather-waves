4f4fa41af01e2290984848975215b98d2926c13d
Chip Carnes <chip.carnes27@gmail.com>
2016-08-30 10:22:18 -0700

    Merge master into chip

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,21 +2,6 @@ import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-<<<<<<< HEAD
-  // model: function() {
-  //   var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=47c99ae9b1f7f7412bba0f762a2b3c68';
-  //   return Ember.$.getJSON(url).then(function(responseJSON) {
-  //     return responseJSON;
-  //   });
-  // },
-  actions: {
-    getLoc: function() {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords.latitude, position.coords.longitude);
-      });
-    },
-
-=======
   model() {
     var key = config.apiKey;
     var url = "http://api.openweathermap.org/data/2.5/forecast?q=Portland,us&mode=json&appid=" + key;
@@ -24,6 +9,12 @@ export default Ember.Route.extend({
       console.log(responseJSON);
       return responseJSON;
     });
->>>>>>> chip
+  },
+  actions: {
+    getLoc: function() {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+      });
+    }
   }
 });
