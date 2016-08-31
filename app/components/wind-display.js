aac7cc7240893632b1c6925dc5b0e141d0b507ad
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-08-31 11:35:40 -0700

    make larger dots move less than smaller dots

--- app/components/wind-display.js
+++ app/components/wind-display.js
@@ -1,24 +1,31 @@
 import Ember from 'ember';
 import d3 from 'd3';
 
-
 export default Ember.Component.extend({
+
   actions: {
     generateWind() {
       document.getElementById("holder").innerHTML = "";
       var windRad = (90-this.get('weather.wind.deg')) * (Math.PI/180) ;
-      let svgContainer1 = d3.select('#holder').append('svg').attr('width',700).attr('height',700);
+      let svgContainer = d3.select('#holder').append('svg').attr('width',700).attr('height',700);
+      var i = 1;
 
-      svgContainer1.append('circle')
-      .attr('cx',250)
-      .attr('cy',250)
-      .attr('r', 100)
-      .style('fill','blue')
-      .transition()
-      .attr('cx', 250 - (250 * Math.cos(windRad)) )
-      .attr('cy', 250 + (250 * Math.sin(windRad)) )
-      .duration(2000)
-      .style('fill','red');
+      while (i++ < 25) {
+        var startX = Math.random()*700;
+        var startY = Math.random()*700;
+        var radius = Math.random()*100;
+        svgContainer.append('circle')
+        .attr('cx',startX)
+        .attr('cy',startY)
+        .attr('r', radius)
+        .style('fill','blue')
+        .transition()
+        .attr('cx', startX - ((100 * Math.cos(windRad))/(radius/100)) )
+        .attr('cy', startY + ((100 * Math.sin(windRad))/(radius/100)) )
+        .duration(2000)
+        .style('fill','red');
+      }
+
     },
     weatherNow: function() {
       console.log(this.get('weather'));
@@ -29,5 +36,6 @@ export default Ember.Component.extend({
       };
       this.sendAction('citySearch', params);
     }
+
   }
 });
