96630f113ee6b865faf5b73cb9faaeab7cbe97cc
Chip Carnes <chip.carnes27@gmail.com>
2016-08-31 08:55:56 -0700

    Moved city comparison code to new route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,4 @@
-import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    var key = config.apiKey;
-    var url = "http://api.openweathermap.org/data/2.5/forecast?q=Portland,us&mode=json&appid=" + key;
-    return Ember.$.getJSON(url).then(function(responseJSON) {
-      console.log(responseJSON);
-      return responseJSON;
-    });
-  },
-  actions: {
-    getLoc: function() {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords.latitude, position.coords.longitude);
-      });
-    }
-  }
 });
