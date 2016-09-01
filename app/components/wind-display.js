6d5a0861032ef1fb45e72ff3a8b6b7e5bc9bb067
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-09-01 09:11:08 -0700

    handle empty object properties in api response gracefully

--- app/components/wind-display.js
+++ app/components/wind-display.js
@@ -17,20 +17,24 @@ export default Ember.Component.extend({
 
   actions: {
 
-    generateWind() {
-      if (!this.get('weather.wind.speed')) {
-        return;
-      } else {
+    generateWeather() {
         document.getElementById("holder").innerHTML = "";
-        var windRad = (90-this.get('weather.wind.deg')) * (Math.PI/180) ;
-        var windSpeed = this.get('weather.wind.speed');
+        var windSpeed = 0;
+        var windRad = 0;
+        if (this.get('weather.wind.speed')) {
+          windSpeed = this.get('weather.wind.speed');
+        };
+        if (this.get('weather.wind.deg')) {
+          windRad = (90-this.get('weather.wind.deg')) * (Math.PI/180);
+        };
         let svgContainer = d3.select('#holder').append('svg').attr('width',window.innerWidth).attr('height',(window.innerHeight - 300));
-        var i = 1;
+        var w = 1;
+        var c = 1;
 
-        while (i++ < 500) {
+        while (w++ < 200) {
           var startX = (Math.random()-Math.random())*window.innerWidth;
           var startY = (Math.random()-Math.random())*window.innerHeight;
-          var radius = Math.random()*12;
+          var radius = Math.random()*4;
           var fullRgb = 'rgb(' + String(Math.floor(Math.random()*255)) + ', ' + String(Math.floor(Math.random()*255)) + ', ' + String(Math.floor(Math.random()*255)) + ')';
           svgContainer.append('rect')
           .attr('x',startX)
@@ -42,8 +46,24 @@ export default Ember.Component.extend({
           .attr('x', startX - (((windSpeed * 60) * Math.cos(windRad))/(radius/120)) )
           .attr('y', startY + (((windSpeed * 60) * Math.sin(windRad))/(radius/120)) )
           .duration(3000);
+        };
+
+        while (c++ < this.get('weather.clouds.all')) {
+          var startX = Math.random()*window.innerWidth;
+          var startY = Math.random()*window.innerHeight;
+          var radius = (2*this.get('weather.clouds.all')) + (Math.random()*(window.innerWidth/30));
+          svgContainer.append('rect')
+          .attr('x',startX)
+          .attr('y',startY)
+          .attr('width', radius)
+          .attr('height', radius)
+          .style('fill', 'white')
+          .style('opacity', '0.8')
+          .transition()
+          .attr('x', startX - (((windSpeed * 60) * Math.cos(windRad))/(radius/120)) )
+          .attr('y', startY + (((windSpeed * 60) * Math.sin(windRad))/(radius/120)) )
+          .duration(3000);
         }
-      }
     },
     weatherNow: function() {
       console.log(this.get('weather'));
