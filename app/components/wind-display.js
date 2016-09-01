2d966b8d428bc7d01f5999f77752a8cbeb33c674
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-09-01 10:03:40 -0700

    shows rainfall if weather code received from api call indicates rain

--- app/components/wind-display.js
+++ app/components/wind-display.js
@@ -21,15 +21,88 @@ export default Ember.Component.extend({
         document.getElementById("holder").innerHTML = "";
         var windSpeed = 0;
         var windRad = 0;
-        if (this.get('weather.wind.speed')) {
-          windSpeed = this.get('weather.wind.speed');
-        };
-        if (this.get('weather.wind.deg')) {
-          windRad = (90-this.get('weather.wind.deg')) * (Math.PI/180);
-        };
+        var weather = this.get('weather');
+        var weatherCode = weather.weather[0].id;
+        console.log(weatherCode);
+        if (weather.wind.speed) {
+          windSpeed = weather.wind.speed;
+        }
+        if (weather.wind.deg) {
+          windRad = (90-weather.wind.deg) * (Math.PI/180);
+        }
         let svgContainer = d3.select('#holder').append('svg').attr('width',window.innerWidth).attr('height',(window.innerHeight - 300));
         var w = 1;
         var c = 1;
+        var r = 1;
+        if (weatherCode >= 200 &&  weatherCode < 300) {
+          //Thunderstorm
+          while (r++ < 200 * (window.innerWidth/1080)){
+            var startX = Math.random()*window.innerWidth;
+            var startY = Math.random()*window.innerHeight;
+            var radius = 5 * Math.random();
+            svgContainer.append('circle')
+            .attr('cx',startX)
+            .attr('cy',startY)
+            .attr('r',radius)
+            .style('fill', 'blue')
+            .transition()
+            .attr('cy', startY+40)
+            .attr('r',0)
+            .duration(1000)
+            .ease('linear');
+          }
+        } else if (weatherCode >= 300 &&  weatherCode < 400 || weatherCode === 701) {
+          //Drizzle
+          while (r++ < 200 * (window.innerWidth/1080)){
+            var startX = Math.random()*window.innerWidth;
+            var startY = Math.random()*window.innerHeight;
+            var radius = 5 * Math.random();
+            svgContainer.append('circle')
+            .attr('cx',startX)
+            .attr('cy',startY)
+            .attr('r',radius)
+            .style('fill', 'blue')
+            .transition()
+            .attr('cy', startY+40)
+            .attr('r',0)
+            .duration(1000)
+            .ease('linear');
+          }
+        } else if (weatherCode >= 500 &&  weatherCode < 600) {
+          //Rain
+          while (r++ < 200 * (window.innerWidth/1080)){
+            var startX = Math.random()*window.innerWidth;
+            var startY = Math.random()*window.innerHeight;
+            var radius = 5 * Math.random();
+            svgContainer.append('circle')
+            .attr('cx',startX)
+            .attr('cy',startY)
+            .attr('r',radius)
+            .style('fill', 'blue')
+            .transition()
+            .attr('cy', startY+40)
+            .attr('r',0)
+            .duration(1000)
+            .ease('linear');
+          }
+        } else if (weatherCode >= 600 &&  weatherCode < 700) {
+          //Snow
+          while (r++ < 200 * (window.innerWidth/1080)){
+            var startX = Math.random()*window.innerWidth;
+            var startY = Math.random()*window.innerHeight;
+            var radius = 5 * Math.random();
+            svgContainer.append('circle')
+            .attr('cx',startX)
+            .attr('cy',startY)
+            .attr('r',radius)
+            .style('fill', 'white')
+            .transition()
+            .attr('cy', startY+40)
+            .attr('r',0)
+            .duration(3000)
+            .ease('linear');
+          }
+        }
 
         while (w++ < 200) {
           var startX = (Math.random()-Math.random())*window.innerWidth;
@@ -46,7 +119,7 @@ export default Ember.Component.extend({
           .attr('x', startX - (((windSpeed * 60) * Math.cos(windRad))/(radius/120)) )
           .attr('y', startY + (((windSpeed * 60) * Math.sin(windRad))/(radius/120)) )
           .duration(3000);
-        };
+        }
 
         while (c++ < this.get('weather.clouds.all')) {
           var startX = Math.random()*window.innerWidth;
